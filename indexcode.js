255968b068e0cb4b7afc31b66828e336631f1dad
com2893 <jalonsoc2012@gmail.com>
2025-04-02 16:22:42 +0200

    code modify

--- indexcode.js
+++ indexcode.js
@@ -1,6 +1,7 @@
-user = ""
-function login(){
 
-    user = prompt("insterte el usuario")
-    console.log(user)
+player_numbers = 0
+
+function create(){
+    player_numbers = prompt("cuantos jugadores desea añadir")
+    nombre_torneo = prompt("nombre del torneo")
 }
