79415bf950a573bb22fa5d4db537ff905f5ca347
com2893 <jalonsoc2012@gmail.com>
2025-04-02 18:04:40 +0200

    modificados:     index.html 	modificados:     indexcode.js 	modificados:     styles.css

--- indexcode.js
+++ indexcode.js
@@ -1,7 +1,7 @@
-
+tournament_name = ""
 player_numbers = 0
 
-function create(){
-    player_numbers = prompt("cuantos jugadores desea añadir")
-    nombre_torneo = prompt("nombre del torneo")
+
+function generate_form(){
+    
 }
