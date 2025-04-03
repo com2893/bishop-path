580d1b99056e00c6c6239e5841c23270e7137a1c
com2893 <jalonsoc2012@gmail.com>
2025-04-03 15:39:55 +0200

    sumari

--- indexcode.js
+++ indexcode.js
@@ -1,7 +1,26 @@
+const form = document.getElementById("formulacion")
 tournament_name = ""
 player_numbers = 0
 
 
 function generate_form(){
+    const formulario = document.getElementById("miFormulario");
+
+    // Evento para agregar un nuevo input cuando se hace clic en el botón
     
+        // Crear un nuevo input
+        let nuevoInput = document.createElement("input");
+
+        // Establecer el tipo de input (por ejemplo, un input de tipo texto)
+        nuevoInput.type = "text";
+
+        // Establecer el atributo `placeholder` para el nuevo input
+        nuevoInput.placeholder = "number of player";
+
+        // Añadir el nuevo input al formulario
+        form.appendChild(nuevoInput);
+
+        // Añadir un salto de línea (opcional)
+        formulario.appendChild(document.createElement("br")); 
+     
 }
